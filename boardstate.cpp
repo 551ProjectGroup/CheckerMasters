aea2d7c36a9ffcc50d686d36fe0414d8189bcd0b
jvm3487 <jvm3487@users.noreply.github.com>
2014-11-09 15:18:45 -0500

    Update boardstate.cpp

--- boardstate.cpp
+++ boardstate.cpp
@@ -1,17 +1,38 @@
 #include "boardstate.h"
+#include <iostream>
+
+void BoardState::changeTurn(void){
+  if (turn == 1){
+    turn = 0;
+  }
+  else{
+    turn = 1;
+  }
+}
+
+Piece BoardState::getPiece(int x, int y){
+  return board[x][y];
+}
 
 bool BoardState::checkLegalJump(UserInput & input){
   int nY = input.getNext().y;
   int nX = input.getNext().x;
   int cY = input.getCurrent().y;
   int cX = input.getCurrent().x;
+  int computerColor;
+  if (userColor == 1){
+    computerColor = 0;
+  }
+  else{
+    computerColor = 1;
+  }
   if ((cY - nY == 2) || (cY - nY == -2)){  
     if ((cX - nX == 2) || (cX - nX == -2)){
-      if (board[cX][cY].getType == 1){
-	if (board[nX][nY].getEmpty() != 0){
+      if (board[cX][cY].getType() == 1){
+	if (board[nX][nY].getEmpty() != 1){
 	  return false;
 	} 
-	if (board[cX + ((cX - nX) /2)][cY - ((cY - nY)/2)].getPieceColor != userColor){
+	if (board[cX - ((cX - nX) /2)][cY - ((cY - nY)/2)].getPieceColor() == computerColor){
 	  return true;
 	}
 	else{
@@ -23,10 +44,10 @@ bool BoardState::checkLegalJump(UserInput & input){
 	  return false; //if going backwards
 	}
 	else{ 
-	  if (board[nX][nY].getEmpty() != 0){
+	  if (board[nX][nY].getEmpty() != 1){
 	    return false;
 	  } 
-	  if (board[cX + ((cX - nX) /2)][cY - ((cY - nY)/2)].getPieceColor != userColor){
+	  if (board[cX - ((cX - nX) /2)][cY - ((cY - nY)/2)].getPieceColor() == computerColor){
 	    return true;
 	  }
 	  else{
@@ -38,6 +59,7 @@ bool BoardState::checkLegalJump(UserInput & input){
     else{
       return false;
     }
+  }
   else{
     return false;
   }
@@ -52,27 +74,27 @@ bool BoardState::checkNoJumps(UserInput & Input){
   }
   for(int i = 0; i < 8; i++){
     for(int j =0; j <8; j++){
-      if (board[i][j].getPieceColor == userColor){
+      if (board[i][j].getPieceColor() == userColor && board[i][j].getEmpty() != 1){
 	if (j>1){
 	  if ( i > 1){
-	    if (board[i-1][j-1].getPieceColor == compColor && board[i-2][j-2].getEmpty == 1){
+	    if (board[i-1][j-1].getPieceColor() == compColor && board[i-2][j-2].getEmpty() == 1){
 	      return false;
 	    }
 	  }
 	  if ( i < 6){
-	    if (board[i+1][j-1].getPieceColor == compColor && board[i+2][j-2].getEmpty == 1){
+	    if (board[i+1][j-1].getPieceColor() == compColor && board[i+2][j-2].getEmpty() == 1){
 	      return false;
 	    }
 	  }
 	}
-	if (j<6 && board[i][j].getType == 1){
+	if (j<6 && board[i][j].getType() == 1){
 	  if ( i > 1){
-	    if (board[i-1][j+1].getPieceColor == compColor && board[i-2][j+2].getEmpty == 1){
+	    if (board[i-1][j+1].getPieceColor() == compColor && board[i-2][j+2].getEmpty() == 1){
 	      return false;
 	    }
 	  }
 	  if ( i < 6){
-	    if (board[i+1][j+1].getPieceColor == compColor && board[i+2][j+2].getEmpty == 1){
+	    if (board[i+1][j+1].getPieceColor() == compColor && board[i+2][j+2].getEmpty() == 1){
 	      return false;
 	    }
 	  }
@@ -83,14 +105,14 @@ bool BoardState::checkNoJumps(UserInput & Input){
   return true;
 }
 bool BoardState::checkRegMove(UserInput & Input){
-  int nY = input.getNext().y;
-  int nX = input.getNext().x;
-  int cY = input.getCurrent().y;
-  int cX = input.getCurrent().x;
+  int nY = Input.getNext().y;
+  int nX = Input.getNext().x;
+  int cY = Input.getCurrent().y;
+  int cX = Input.getCurrent().x;
   if ((cY - nY == 1) || (cY - nY == -1)){  
     if ((cX - nX == 1) || (cX - nX == -1)){
-      if (board[cX][cY].getType == 1){
-	if (board[nX][nY].getEmpty() != 0){
+      if (board[cX][cY].getType() == 1){
+	if (board[nX][nY].getEmpty() != 1){
 	  return false;
 	}
 	return true;
@@ -99,17 +121,18 @@ bool BoardState::checkRegMove(UserInput & Input){
 	if (nY - cY == 1){ //going backward
 	  return false; 
 	}
-	if (board[nX][nY].getEmpty() != 0){
+	if (board[nX][nY].getEmpty() != 1){
 	  return false;
 	}
 	return true;
       }
     }
   }
+  return false;
 }
 
 bool BoardState::checkLegality(UserInput & input){
-  if (board[input.getCurrent().x][input.getCurrent().y].getPieceColor != userColor){
+  if (board[input.getCurrent().x][input.getCurrent().y].getPieceColor() != userColor){
     return false;
   }
   //check jump
@@ -138,7 +161,7 @@ int BoardState::getTurn(void){
   return turn;
 }
 
-void BoardState::BoardState(int color){
+BoardState::BoardState(int color){
   turn = color;
   userColor = color;
   int computerColor;
@@ -150,35 +173,43 @@ void BoardState::BoardState(int color){
   }
   for(int i = 0; i < 8; i=i+2){
     for(int j = 0; j < 8; j=j+2){
-      board[i][j] = Piece();
+      Piece * newPiece = new Piece();
+      board[j][i] = *newPiece;
     }
   }
   for(int i = 1; i < 8; i=i+2){
     for(int j = 1; j < 8; j=j+2){
-      board[i][j] = Piece();
+      Piece * newPiece = new Piece();
+      board[j][i] = *newPiece;
     }
   } 
   for (int j = 0; j < 8; j=j+2){
-    board[3][j] = Piece();
+    Piece * newPiece = new Piece();
+    board[j][3] = *newPiece;
   }
   for (int j = 1; j < 8; j=j+2){
-    board[4][j] = Piece();
+    Piece * newPiece = new Piece();
+    board[j][4] = *newPiece;
   }
   for (int i = 5; i < 8; i = i+2){
     for (int j = 0; j < 8; j= j+2){
-      board[i][j] = Piece(color);
+      Piece * userPiece = new Piece(color);
+      board[j][i] = *userPiece;
     }
   }
   for(int j = 1; j< 8; j=j+2){
-    board[6][j] = Piece(color);
+    Piece * userPiece = new Piece(color);
+    board[j][6] = *userPiece;
   }
   for (int i = 0; i < 3; i = i+2){
     for (int j = 1; j < 8; j= j+2){
-      board[i][j] = Piece(computerColor);
+      Piece * computerPiece = new Piece(computerColor);
+      board[j][i] = *computerPiece;
     }
   }
   for(int j = 0; j< 8; j=j+2){
-    board[1][j] = Piece(computerColor);
+    Piece * computerPiece = new Piece(computerColor);
+    board[j][1] = *computerPiece;
   }
 }
 
@@ -188,12 +219,20 @@ void BoardState::updateBoard(UserInput & input){
   int cY = input.getCurrent().y;
   int cX = input.getCurrent().x;
   if (nY == 0 && board[cX][cY].getType() != 1){
-    board[nX][nY] = Piece(board[cX][cY].getColor(), 1, 1);
+    Piece * update = new Piece(board[cX][cY].getPieceColor(), 1, 1);
+    board[nX][nY] = *update;
+    delete update;
   }
-  board[nX][nY] = board[cX][cY];
-  board[cX][cY] = Piece();
+  else{
+    board[nX][nY] = board[cX][cY];
+  }
+  Piece * newPiece = new Piece();
+  board[cX][cY] = *newPiece;
+  delete newPiece;
   if (cX - nX == -2 || cX - nX == 2){
-    board[cX + ((cX - nX) /2)][cY - ((cY - nY)/2)] = Piece();
+    Piece * deletePiece = new Piece();
+    board[cX - ((cX - nX) /2)][cY - ((cY - nY)/2)] = *deletePiece;
+    delete deletePiece;
   }
 }
 
@@ -202,29 +241,42 @@ bool BoardState::checkFurther(UserInput & input){
   int nX = input.getNext().x;
   int cY = input.getCurrent().y;
   int cX = input.getCurrent().x;
+  int compColor;
+  if (userColor == 1){
+    compColor = 0;
+  }
+  else{
+    compColor = 1;
+  }
+  if (board[nX][nY].getUpgrade() == 1){
+    Piece * update = new Piece(board[nX][nY].getPieceColor(), 1, 0);
+    board[nX][nY] = *update;
+    delete update;
+    return false;
+  }
   if (cX - nX == 1 || cX - nX == -1){
     return false;
   }
   if (nY > 1){
     if ( nX > 1){
-      if (board[nX-1][nY-1].getPieceColor == compColor && board[nX-2][nY-2].getEmpty == 1){
+      if (board[nX-1][nY-1].getPieceColor() == compColor && board[nX-2][nY-2].getEmpty() == 1){
 	return true;
       }
     }
     if ( nX < 6){
-      if (board[nX+1][nY-1].getPieceColor == compColor && board[nX+2][nY-2].getEmpty == 1){
+      if (board[nX+1][nY-1].getPieceColor() == compColor && board[nX+2][nY-2].getEmpty() == 1){
 	return true;
       }
     }
   }
-  if (nY <6 && board[cX][cY].getType == 1){
+  if (nY <6 && board[cX][cY].getType() == 1){
     if ( nX > 1){
-      if (board[nX-1][nY+1].getPieceColor == compColor && board[nX-2][nY+2].getEmpty == 1){
+      if (board[nX-1][nY+1].getPieceColor() == compColor && board[nX-2][nY+2].getEmpty() == 1){
 	return true;
       }
     }
     if ( nX < 6){
-      if (board[nX+1][nY+1].getPieceColor == compColor && board[nX+2][nY+2].getEmpty == 1){
+      if (board[nX+1][nY+1].getPieceColor() == compColor && board[nX+2][nY+2].getEmpty() == 1){
 	return true;
       }
     }
