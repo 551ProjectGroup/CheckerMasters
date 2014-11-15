d2991d6823b87195074bcd61d905fb96c47f0a71
AshwinDandekar <dandekar.ashwin@gmail.com>
2014-11-15 00:02:19 -0500

    Create newgame.h

---
+++ newgame.h
@@ -0,0 +1,31 @@
+#ifndef NEWGAME_H
+#define NEWGAME_H
+#include <QThread>
+#include <QGraphicsScene>
+#include "boardstate.h"
+
+class NewGame : public QThread
+{
+private:
+Q_OBJECT                                        //This guy is needed to start a thread. This thread will run our game without stopping.
+int difficulty;                                 //Difficulty
+int color;                                      //user colour.
+BoardState *gamestate;
+
+public:
+virtual void run();
+explicit NewGame(QObject *parent = 0);          //a method needed for threads.
+NewGame(): difficulty(0), color(0) {}
+NewGame(int d, int c): difficulty(d), color(c) {}
+NewGame(NewGame &rhs);
+int getPieceColor(void);                        //Getters and setters.
+void setDifficulty(int d);
+int getDifficulty(void);
+void setPieceColor(int color);
+                    //This guy renders the board pieces and gives back a board.
+
+BoardState *getGamestate() const;
+void setGamestate(BoardState *value);
+};
+
+#endif // NEWGAME_H
