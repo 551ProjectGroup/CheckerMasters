0cc673f51b398007613e06490b393ff5b83f8177
jvm3487 <jvm3487@users.noreply.github.com>
2014-11-17 17:18:13 -0500

    Update newgame.cpp

--- newgame.cpp
+++ newgame.cpp
@@ -12,12 +12,35 @@ void NewGame::setGamestate(BoardState *value)
 {
     gamestate = value;
 }
-void NewGame::run()
+
+void NewGame::winLoss(int val)
 {
-    //Inifinite loop to be made here.
+    winloss = val;
 }
 
-NewGame::NewGame(NewGame &rhs) {
+void NewGame::compplays(int play) {
+    compturn = play;
+}
+
+void NewGame::run()
+{
+   /* QMutex mutex;
+    mutex.lock();
+    while(1) {
+    Call methods to make the computer play. At the end of that method the turn is changed.
+
+    if(winloss) {
+        emit decision(winloss);
+        break;
+    }
+    compplays.wait(&mutex);
+     This thread is sleeping till its the comps turn to play again.
+     That way we wont be executing the inifinite loop over and over again.
+     We wake this thread up at the end of the users turn check method
+    }*/
+}
+
+NewGame::NewGame(NewGame &rhs) : QThread() {
     color = rhs.color;
     difficulty = rhs.difficulty;
 }
